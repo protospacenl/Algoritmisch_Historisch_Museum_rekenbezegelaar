f3a59b46981c92905e3cb6d00bf2459deb2fed34
JJS <jan-jaap_schuurman@hotmail.com>
2019-08-27 16:41:52 +0200

    added comment for setting the jumpers and timeout

--- Code/Buttonduispenser_final/Buttonduispenser_final.ino
+++ Code/Buttonduispenser_final/Buttonduispenser_final.ino
@@ -1,5 +1,13 @@
 /* 
  *  Dispenser control V1.0 for AHM
+ *  Jan-Jaap Schuurman | ProtoSpace 2019  
+ *  User wait time between 2 dispenses are configurable by pressing the dispense button during startup/power-on. Setting the male and female jackinput as jumper setting:
+ *  |MALE |FEMALE|
+ *  |     |      |  10 SEC. |  
+ *  |CHECK|      |  20 SEC. |
+ *  |     |CHECK |  30 SEC. |
+ *  |CHECK|CHECK |  60 SEC. |
+ *
  */
 
 #include <Servo.h>
